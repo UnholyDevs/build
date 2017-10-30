379288783b14a9e61cf59ff7dd19e08eafa11522
ezio84 <brabus84@gmail.com>
2017-10-30 12:36:58 +0100

    Switch to Pixel2XL 8.1 sounds [2/2]
    Change-Id: I8299216c4bd50b9791d6f0f3420f1a5aba34f2cd

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -39,8 +39,7 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Kill_Or_Become.ogg \
-    ro.config.notification_sound=Chime.ogg
+    ro.config.ringtone=Kill_Or_Become.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
